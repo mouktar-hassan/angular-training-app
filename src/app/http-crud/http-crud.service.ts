276786dff37ac3f435ef1bda01e67f172eeff524
hassan-farah <mouktar.hassan.farah@outlook.fr>
2024-01-22 14:21:26 +0100

    configuratio,

--- src/app/http-crud/http-crud.service.ts
+++ src/app/http-crud/http-crud.service.ts
@@ -9,7 +9,7 @@ export class httpServices {
   constructor(private http: HttpClient) { }
 
   getAllEmployee(){
-    const url = 'localhost:8080/api/users/';
+    const url = 'http://localhost:8080/api/users/';
     return this.http.get(url, {responseType: 'json'})
     .pipe(
         tap( //log the result or error
@@ -21,10 +21,36 @@ export class httpServices {
     )
   }
 
-  private log(url: string, data:string| any){
-    const message = `Récupération des employés à travers l\'url : "${url}", voici les données réçus "${data}".`;
-    console.log(message);
+  getOneEmployee(id: number){
+    const url = `http://localhost:8080/api/users/${id}`;
+    return this.http.get(url, {responseType: 'json'})
+    .pipe(
+        tap( //log the result or error
+        {
+            next: (data) => this.log(url, data),
+            error: (error) => this.logError(url, error)
+        }
+        )
+    )
   }
+
+  addEmployee(employee : any){
+
+  }
+
+  updateEmployee(id: number){
+
+  }
+
+  deleteEmployee(id: number){
+
+  }
+
+  private log(url: string, data: any) {
+    const message = `Récupération des employés à travers l'url : "${url}", voici les données reçues : ${JSON.stringify(data)}`;
+    console.log(message);
+}
+
 
   private logError(url: string, error:any){
     const message = `Erreur de récupération lors de tentative de l\'url : "${url}"; causé par "${error.message}".`;
